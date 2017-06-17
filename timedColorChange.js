180c16442277df7af1c0136679e67519ef2eb7af
Taniya Prabhakar <tprabhakar@ucdavis.edu>
2017-06-17 10:41:48 -0700

    added useful comments

--- timedColorChange.js
+++ timedColorChange.js
@@ -1,9 +1,10 @@
 var used = [];
 
 function getRandomColor() {
-    var letters = '0123456789ABCDEF';
     var color = '#';
+    var letters = '0123456789ABCDEF';
 
+    // generates random color string
     for( var i = 0; i < 6; i++ ) {
         color += letters[Math.floor( Math.random() * 16 )];
     }
@@ -13,17 +14,19 @@ function getRandomColor() {
 function setRandomColor() {
     var background = document.getElementsByTagName( 'main' )[0];
     var newColor = getRandomColor();
-
+    // checks if generated color has already been used
     if( used.includes( newColor ) ) {
+        // if yes, sets a new value for color
         newColor = getRandomColor();
     }
 
     background.style.backgroundColor = newColor;
+    // keeps track of colors used
     used.push( newColor );
 }
-
+// changes background color every second
 var changeBackground = setInterval( setRandomColor, 1000 );
-
+// stops changing color after 10 seconds
 setTimeout( function() {
     clearInterval( changeBackground );
 }, 10000 );
